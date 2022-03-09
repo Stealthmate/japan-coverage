bc672a458ebfc12469b17bbe2d6b5ff50ff7501f
stealthmate <stealthmate1233@gmail.com>
2022-03-09 22:39:42 +0900

    Add build_data script

---
+++ build_data.py
@@ -0,0 +1,57 @@
+import os.path
+import json
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1PkzQxsYZYHhimos5H6IDW5az8HjZ9_8NDy-3LpjJonU'
+SAMPLE_RANGE_NAME = 'History!A2:G1000'
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=12345)
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+        with open('web/src/assets/data.json', mode='w', encoding='utf-8') as f:
+          json.dump([
+            dict(
+              prefecture=row[2],
+              city=row[3],
+              place=row[4],
+              location=[float(row[0]), float(row[1])]
+            )
+            for row in values
+          ], f, ensure_ascii=False)
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    main()
