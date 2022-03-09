d9f0920ea6762253529cad25bdf4479b00651b71
stealthmate <stealthmate1233@gmail.com>
2022-03-09 22:46:39 +0900

    Fix bug

--- web/src/api.js
+++ web/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetchCityOverlay = async () => {
-  let response = await axios.get('/overlays/japan.svg/');
+  let response = await axios.get('./overlays/japan.svg/');
   return response.data;
 };
